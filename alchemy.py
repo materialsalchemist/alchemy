79a22d74869fb242c2dc4b200b66d4354ca42daf
newbee1905 <beenewminh@outlook.com>
2025-09-16 10:49:00 +1000

    feat/wip: attempt to fix multiprocessing

--- alchemy.py
+++ alchemy.py
@@ -1,19 +1,20 @@
 import click
-from multiprocessing import set_start_method
+from multiprocessing import set_start_method, freeze_support
 from chemical_space.cli import chemical_cli
 from reaction_space.cli import reaction_cli
 
 @click.group()
 def cli():
-    """Main CLI for alchemy."""
-    pass
+	"""Main CLI for alchemy."""
+	pass
 
 cli.add_command(chemical_cli, name="chemical")
 cli.add_command(reaction_cli, name="reaction")
 
 def main():
+	set_start_method("spawn")
 	cli()
 
 if __name__ == "__main__":
-	set_start_method("spawn")
+	freeze_support()
 	main()
